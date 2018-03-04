31b8ab18c63bf47633d67e53717c428b7fd638b1
BrookeMa <maye634028187@icloud.com>
2018-03-04 14:32:45 +0800

    添加sass

---
+++ dev_dependent/complier-sass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sass index.scss ../pages/index/index.wxss
+sass answer_detail.scss ../pages/answer_detail/answer_detail.wxss
+sass paper_selection.scss ../pages/paper_selection/paper_selection.wxss
+sass province_selection.scss ../pages/province_selection/province_selection.wxss
